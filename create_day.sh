a317ab705c6f3a167643553ec4de2e8bef76ce2f
Oscar Lostes Cazorla <oscar.lostes@e-campus.uab.cat>
2021-12-10 13:15:31 +0100

    Create day script refactor

--- create_day.sh
+++ create_day.sh
@@ -5,7 +5,8 @@ cd "day"$1
 
 touch input.txt
 touch example.txt
-touch get_input.py
+
+echo -e 'def get_input(filename="input.txt"):\n\twith open(filename, "r") as f:' > get_input.py
 
 echo -e "#!/usr/bin/env python3\n# -*- coding: utf-8 -*-\n\nfrom get_input import get_input" > puzzle1.py
 chmod +x puzzle1.py
